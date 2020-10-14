c0603f1ad86254f40c0591d9e160dca5363b4f0e
NicaGit <nicagit@test.com>
2020-10-14 18:32:51 +0800

    This is the initial front-end files.

---
+++ create-order.py
@@ -0,0 +1,17 @@
+## load JSON file from API response
+import json
+
+def lambda_handler(event, context):
+    ## Initialize variables
+    for key in event:
+        if 'name' in key:
+            CustName = key['name']
+        if 'number' in key:
+            CustNum = key['number'] 
+
+
+
+
+## move contact details to S3-JSON file
+## add and move orders to S3-JSON file
+## upload S3-JSON file
